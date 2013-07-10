4d1ec029d3b4e0ccd3821d631b4f47aae9ac1e23
Eugene Kibalko <evgkib@hotmail.com>
2013-07-10 09:56:37 -0500

    Modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
+var file = fs.readFileSync('index.html');
+var buf = new Buffer(file);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log(buf.toString());
 });
