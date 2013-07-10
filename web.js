f18f4f976b9a8f287068a8f17d0a2685e2c22e5f
Eugene Kibalko <evgkib@hotmail.com>
2013-07-10 10:50:02 -0500

    Modified web.js

--- web.js
+++ web.js
@@ -8,8 +8,7 @@ app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-  console.log(buf.toString());
-});
+ });
